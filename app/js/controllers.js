eac40f08bedee5cb8db8f8f46d97b4d25cb8c716
Fabian Linzberger <e@lefant.net>
2014-05-02 21:02:28 +0200

    add photoCtrl. bind simple search result and implement tagging and untagging prototype.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,41 @@
 'use strict';
 
-/* Controllers */
+angular.module('flickrDupFinderControllers',
+               ['flickrDupFinderServices', 'underscore'])
+  .controller(
+    'photoCtrl',
+    ['$scope', '$log', 'Flickr', '_',
+     function($scope, $log, Flickr, _) {
+       Flickr.get({tags: "screens"}, function(result){
+         $scope.results = result.photos.photo;
+       });
+       Flickr.get({tags: "flickrdupfinder"}, function(result){
+         $scope.specials = result.photos.photo;
+       });
 
-angular.module('flickrDupFinder.controllers', [])
-  .controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+       $scope.toggleTag = function(photo) {
+         if (_.contains(photo.tags.split(" "), 'flickrdupfinder')) {
+           Flickr.get({
+             method: 'flickr.photos.getInfo',
+             photo_id: photo.id,
+             tags: 'flickrdupfinder'
+           }, function(info) {
+             var tag = _.find(info.photo.tags.tag,
+                              function(tag) {
+                                return tag.raw === 'flickrdupfinder';
+                              });
+             Flickr.get({
+               method: 'flickr.photos.removeTag',
+               photo_id: photo.id,
+               tag_id: tag.id
+             });
+           });
+         } else {
+           Flickr.get({
+             photo_id: photo.id,
+             method: 'flickr.photos.addTags',
+             tags: 'flickrdupfinder'
+           });
+         }
+       };
+     }]);
