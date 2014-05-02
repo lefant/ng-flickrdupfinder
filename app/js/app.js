dc4648787f549d720b044657d614786aa3a9aafa
Fabian Linzberger <e@lefant.net>
2014-05-02 20:58:34 +0200

    update app.js with module names, routes and add underscore stub

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,21 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
 angular.module('flickrDupFinder', [
   'ngRoute',
-  'flickrDupFinder.filters',
-  'flickrDupFinder.services',
-  'flickrDupFinder.directives',
-  'flickrDupFinder.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'flickrDupFinderServices',
+  'flickrDupFinderControllers'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/photos', {
+        templateUrl: 'partials/photos.html',
+        controller: 'photoCtrl'
+      })
+    .otherwise({
+      redirectTo: '/photos'
+    });
+  }]);
+
+
+angular.module('underscore', []).factory('_', function() {
+  return window._; // assumes underscore has already been loaded on the page
+});
