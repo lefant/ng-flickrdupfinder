a83fad6735ce14548c3cc2927a7884826fb7e61a
Fabian Linzberger <e@lefant.net>
2014-07-20 17:19:27 +0200

    update oauth flickr wrapper for latest api, use nisse oauthd
    once https://github.com/oauth-io/oauthd/pull/101 is merged and deployed
    on oauth.io we can switch back to using that instead of nisse.

--- app/js/services.js
+++ app/js/services.js
@@ -4,22 +4,36 @@
 
 angular.module('flickrDupFinderServices', ['ngResource'])
   .factory('Flickr', ['$window', '$log', '$resource', '$http', function($window, $log, $resource, $http) {
-    var key = 'cF4gOblEUpueTtsL44-gVjZeeXM';
+    var oauthd_url = 'http://nisse.lefant.net:6284';
+    var key = 'foRRKXfQipy7kziBuWDhh1Ibs_k';   // nisse
+    //var key = 'cF4gOblEUpueTtsL44-gVjZeeXM'; // oauth.io
     $window.OAuth.initialize(key, {cache: true});
+    $window.OAuth.setOAuthdURL(oauthd_url);
     var resource;
-    $window.OAuth.popup('flickr', {state: 'state'}, function(error, result) {
-      if(error){ $log.log('OAuth.popup error: ', error); };
-      var oauthio_endpoint = 'https://oauth.io/request/flickr/services/rest/';
+    $window.OAuth.popup('flickr', {state: 'state'}).done(function(result) {
+      // test request
+      result.get('services/rest',
+                 {data: {
+                   method: "flickr.photos.search",
+                   format: "json",
+                   user_id: "me",
+                   per_page: 10,
+                   extras: "date_upload,date_taken,tags",
+                   nojsoncallback: 1
+                 }}).done(function(res) {
+                   $log.log('initial OAuth result.get() successful: ', res);
+                 }).fail(function(err) {
+                   $log.log('initial OAuth result.get() failure: ', err);
+                 });
+
       var oauthio = 'k=' + key;
       oauthio += '&oauthv=1';
-      function kv_result(key) {
-        return '&'+key+'='+encodeURIComponent(result[key])
-      }
+      function kv_result(key) { return '&'+key+'='+encodeURIComponent(result[key]); }
       oauthio += kv_result('oauth_token');
       oauthio += kv_result('oauth_token_secret');
       oauthio += kv_result('code');
       $http.defaults.headers.common = {oauthio: oauthio};
-      resource = $resource('https://oauth.io/request/flickr/services/rest/',
+      resource = $resource(oauthd_url + '/request/flickr/services/rest/',
                            {
                              method: "flickr.photos.search",
                              format: "json",
@@ -31,6 +45,10 @@ angular.module('flickrDupFinderServices', ['ngResource'])
                              extras: "date_upload,date_taken,tags",
                              nojsoncallback: 1
                            });
+
+
+    }).fail(function(error) {
+      $log.log('OAuth.popup error: ', error);
     });
     return resource;
   }]);
