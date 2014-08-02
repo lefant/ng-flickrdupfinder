6fd598c0407fdf5984efbab24d38de31f7945701
Fabian Linzberger <e@lefant.net>
2014-08-02 14:19:33 +0200

    object instead of array for photos in $scope. add/remove instead of toggle.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,36 +6,51 @@ angular.module('flickrDupFinderControllers',
     'photoCtrl',
     ['$scope', '$log', 'Flickr', '_',
      function($scope, $log, Flickr, _) {
-       Flickr.get({tags: "screens"}, function(result){
-         $scope.results = result.photos.photo;
-       });
-       Flickr.get({tags: "flickrdupfinder"}, function(result){
-         $scope.specials = result.photos.photo;
-       });
+       $scope.addTag = function(photo) {
+         Flickr.get({
+           photo_id: photo.id,
+           method: 'flickr.photos.addTags',
+           tags: 'flickrdupfinder'
+         });
+         delete $scope.results[photo.id];
+         $scope.specials[photo.id] = photo;
+       };
 
-       $scope.toggleTag = function(photo) {
-         if (_.contains(photo.tags.split(" "), 'flickrdupfinder')) {
-           Flickr.get({
-             method: 'flickr.photos.getInfo',
-             photo_id: photo.id,
-             tags: 'flickrdupfinder'
-           }, function(info) {
-             var tag = _.find(info.photo.tags.tag,
-                              function(tag) {
-                                return tag.raw === 'flickrdupfinder';
-                              });
+       $scope.removeTag = function(photo) {
+         Flickr.get({
+           method: 'flickr.photos.getInfo',
+           photo_id: photo.id,
+           tags: 'flickrdupfinder'
+         }, function(info) {
+           var tag = _.find(info.photo.tags.tag,
+                            function(tag) {
+                              return tag.raw === 'flickrdupfinder';
+                            });
+           if (tag) {
              Flickr.get({
                method: 'flickr.photos.removeTag',
                photo_id: photo.id,
                tag_id: tag.id
              });
-           });
-         } else {
-           Flickr.get({
-             photo_id: photo.id,
-             method: 'flickr.photos.addTags',
-             tags: 'flickrdupfinder'
-           });
-         }
+           }
+         });
+         delete $scope.specials[photo.id];
+         $scope.results[photo.id] = photo;
        };
+
+       function objectifyResult(result) {
+         function prefixId(photo) {
+           return [photo.id, photo];
+         }
+         var prefixedResult = _.map(result, prefixId);
+         return _.object(prefixedResult);
+       }
+
+       Flickr.get({tags: "flickrdupfinder"}, function(specialResult) {
+         $scope.specials = objectifyResult(specialResult.photos.photo);
+         Flickr.get({tags: "screens", per_page: 10}, function(plainResult) {
+           var allResults = objectifyResult(plainResult.photos.photo);
+           $scope.results = _.omit(allResults, _.keys($scope.specials));
+         });
+       });
      }]);
