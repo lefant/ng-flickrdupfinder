71cc7c4c89c52c668eb37a3d351f19016318e3ef
Fabian Linzberger <e@lefant.net>
2014-08-03 20:48:38 +0200

    find duplicates, toggle state and update display via css

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,26 +6,35 @@ angular.module('flickrDupFinderControllers',
     'photoCtrl',
     ['$scope', '$log', 'Flickr', '_',
      function($scope, $log, Flickr, _) {
-       $scope.addTag = function(photo) {
+       var specialTag = 'flickrdupfinder';
+
+       $scope.toggleTag = function(photo) {
+         if (photo.duplicate) {
+           removeTag(photo);
+         } else {
+           addTag(photo);
+         }
+       };
+
+       function addTag(photo) {
+         $log.debug("addTag: ", photo);
          Flickr.get({
            photo_id: photo.id,
-           method: 'flickr.photos.addTags',
-           tags: 'flickrdupfinder'
+           method: 'flickr.photos.addTags'
          }, function() {
-           delete $scope.results[photo.id];
+           photo.duplicate = true;
            $scope.specials[photo.id] = photo;
          });
        };
 
-       $scope.removeTag = function(photo) {
+       function removeTag(photo) {
          Flickr.get({
            method: 'flickr.photos.getInfo',
-           photo_id: photo.id,
-           tags: 'flickrdupfinder'
+           photo_id: photo.id
          }, function(info) {
            var tag = _.find(info.photo.tags.tag,
                             function(tag) {
-                              return tag.raw === 'flickrdupfinder';
+                              return tag.raw === specialTag;
                             });
            if (tag) {
              Flickr.get({
@@ -33,18 +42,33 @@ angular.module('flickrDupFinderControllers',
                photo_id: photo.id,
                tag_id: tag.id
              }, function() {
+               photo.duplicate = false;
                delete $scope.specials[photo.id];
-               $scope.results[photo.id] = photo;
              });
            }
          });
        };
 
+       function checkTag(photo) {
+         photo['duplicate'] = _.contains(photo.tags.split(/ /), specialTag);
+         return photo;
+       }
+
+       function fingerprint(photo) {
+         return photo.datetaken + '+' + photo.title;
+       }
+
+       function atLeastTwo(group) {
+         return group[1].length > 1;
+       }
+
        Flickr.get({tags: "flickrdupfinder"}, function(specialResult) {
          $scope.specials = _.indexBy(specialResult.photos.photo, 'id');
          Flickr.get({tags: "screens", per_page: 10}, function(plainResult) {
-           var allResults = _.indexBy(plainResult.photos.photo, 'id');
-           $scope.results = _.omit(allResults, _.keys($scope.specials));
+           var allResults = _.map(plainResult.photos.photo, checkTag);
+           var groups = _.groupBy(allResults, fingerprint);
+           var groups2 = _.object(_.filter(_.pairs(groups), atLeastTwo));
+           $scope.groups = groups2;
          });
        });
      }]);
