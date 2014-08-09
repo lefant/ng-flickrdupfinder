29b8196468fb68a5240148c3685424ef5e0cbfad
Fabian Linzberger <e@lefant.net>
2014-08-09 13:34:27 +0200

    gulp: make deploy depend on build

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var deploy = require("gulp-gh-pages");
 
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
     gulp.src("./build/**/*")
         .pipe(deploy({}));
 });
