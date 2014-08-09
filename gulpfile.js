76dc6f42c3f65fb64be572814449289aa84651c9
Fabian Linzberger <e@lefant.net>
2014-08-09 08:46:11 +0200

    gulp from gulp-start

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,12 @@
-var gulp = require("gulp");
-var gutil = require('gulp-util');
-var source = require("vinyl-source-stream");
-var browserify = require("browserify");
-var debowerify = require("debowerify");
-var watchify = require('watchify');
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible
+  for creating multiple tasks, each task has been broken out into
+  its own file in gulp/tasks. Any file in that folder gets automatically
+  required by the loop in ./gulp/index.js (required below).
 
-gulp.task("browserify", function () {
-  return browserify("./app/js/app.js",
-                    { debug: true,
-                      insertGlobals:  true })
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("./dist/"));
-});
+  To add a new task, simply add a new task file to gulp/tasks.
+*/
 
-
-gulp.task("watch", function () {
-  var bundler = watchify(browserify('./app/js/app.js', watchify.args));
-
-  // Optionally, you can apply transforms
-  // and other configuration options on the
-  // bundler just as you would with browserify
-  //bundler.transform('brfs')
-
-  bundler.on('update', rebundle)
-
-  function rebundle () {
-    return bundler.bundle()
-      // log errors if they happen
-      .on('error', function(e) {
-        gutil.log('Browserify Error', e);
-      })
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./dist'))
-  }
-
-  return rebundle()
-})
+require('./gulp');
