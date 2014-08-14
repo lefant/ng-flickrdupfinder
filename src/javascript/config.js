c6294b135a66c1620186ed95b53304ba307b4711
Fabian Linzberger <e@lefant.net>
2014-08-14 08:48:39 +0200

    change nisse public key

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -2,5 +2,5 @@
 
 module.exports = angular.module('flickrDupFinderConfig', [])
   .constant('OAUTHD_URL', 'http://nisse.lefant.net:6284')
-  .constant('APP_PUBLIC_KEY', 'foRRKXfQipy7kziBuWDhh1Ibs_k') //nisse
+  .constant('APP_PUBLIC_KEY', 'rIg9b6N2-6s3OGtKboyccVnUfOw') //nisse
   //.constant('APP_PUBLIC_KEY', 'cF4gOblEUpueTtsL44_k') //oauth.io
