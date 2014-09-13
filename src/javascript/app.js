b5b76b72a7b8e367a4aa3db051c3dbcd0491d764
Fabian Linzberger <e@lefant.net>
2014-09-13 19:41:44 +0200

    enable html5 routing without #

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,16 +3,20 @@
 require('angular-route');
 
 angular.module('flickrDupFinder', ['ngRoute', require('./controllers').name])
-  .config(['$routeProvider', function($routeProvider) {
-    // the oauth redirect callback page must be matched with .otherwise
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/start.html',
-        controller: 'startCtrl'
-      })
-      .otherwise({
-        templateUrl: 'partials/photos.html',
-        controller: 'photoCtrl',
-        resolve: { 'Flickr': 'Flickr' }
-      });
-  }]);
+  .config(
+    ['$locationProvider', '$routeProvider',
+     function($locationProvider, $routeProvider) {
+       $locationProvider.html5Mode(true);
+
+       // the oauth redirect callback page must be matched with .otherwise
+       $routeProvider
+         .when('/', {
+           templateUrl: 'partials/start.html',
+           controller: 'startCtrl'
+         })
+         .otherwise({
+           templateUrl: 'partials/photos.html',
+           controller: 'photoCtrl',
+           resolve: { 'Flickr': 'Flickr' }
+         });
+     }]);
