bcf8ed7a5477e5b046472f94d65b86c7361fe4e4
Fabian Linzberger <e@lefant.net>
2015-02-28 13:32:36 +0100

    remove unused jQuery and bootstrap javascript

--- src/javascript/oauth-shim.js
+++ src/javascript/oauth-shim.js
@@ -2,9 +2,6 @@
 
 module.exports = angular.module('OAuth', [])
   .factory('OAuth', ['$window', '$log', function($window, $log) {
-  // jquery from cdn via index.html for now
-  // var jQuery = require('jquery');
-  // global.jQuery = jQuery;
   require('oauth-js');
   return $window.OAuth;
 }]);
