ebc94c97a5a8024b0582a00f00f253a76939d80d
Fabian Linzberger <e@lefant.net>
2015-02-28 13:15:59 +0100

    remove unused gulp-ruby-sass

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,6 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch('src/sass/**', ['sass']);
   gulp.watch('src/images/**', ['images']);
   gulp.watch('src/htdocs/**', ['markup']);
 });
