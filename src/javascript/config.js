8eb195a6b53d0e026783174bb2838ca59f7244a8
Fabian Linzberger <e@lefant.net>
2015-05-03 14:39:17 +0200

    update oauthd-lefant key after server upgrade

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -2,6 +2,6 @@
 
 module.exports = angular.module('flickrDupFinderConfig', [])
   .constant('OAUTHD_URL', 'https://oauthd-lefant.herokuapp.com')
-  .constant('APP_PUBLIC_KEY', 'QqSxC9FpX5QsfRhGPpf68w2gLRE') //oauthd-lefant
+  .constant('APP_PUBLIC_KEY', '4UET5aV8f_Np4Eam-BCfQ8zvNzI') //oauthd-lefant
   //.constant('OAUTHD_URL', 'http://oauth.io')
   //.constant('APP_PUBLIC_KEY', 'cF4gOblEUpueTtsL44-gVjZeeXM') //oauth.io
