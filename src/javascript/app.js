a7a06ac0d0c9e72be737edd0df3724a1ac913b48
Fabian Linzberger <e@lefant.net>
2021-02-21 15:57:42 +0100

    use manual require and 'name' for angular DI

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,23 +1,28 @@
-'use strict';
+'use strict'
 
-require('angular-route');
+require('angular-route')
 
-angular.module('flickrDupFinder', ['ngRoute', require('./controllers').name])
-  .config(
-    ['$locationProvider', '$routeProvider',
-     function($locationProvider, $routeProvider) {
-       //probably breaks things due to oauth redirect landing page hack below
-       //$locationProvider.html5Mode(true);
+require('./controllers')
 
-       // the oauth redirect callback page must be matched with .otherwise
-       $routeProvider
-         .when('/', {
-           templateUrl: 'partials/start.html',
-           controller: 'startCtrl'
-         })
-         .otherwise({
-           templateUrl: 'partials/photos.html',
-           controller: 'photoCtrl',
-           resolve: { 'Flickr': 'Flickr' }
-         });
-     }]);
+angular
+  .module('flickrDupFinder', ['ngRoute', 'flickrDupFinderControllers'])
+  .config([
+    '$locationProvider',
+    '$routeProvider',
+    function ($locationProvider, $routeProvider) {
+      //probably breaks things due to oauth redirect landing page hack below
+      //$locationProvider.html5Mode(true);
+
+      // the oauth redirect callback page must be matched with .otherwise
+      $routeProvider
+        .when('/', {
+          templateUrl: 'partials/start.html',
+          controller: 'startCtrl',
+        })
+        .otherwise({
+          templateUrl: 'partials/photos.html',
+          controller: 'photoCtrl',
+          resolve: { Flickr: 'Flickr' },
+        })
+    },
+  ])
--- src/javascript/services.js
+++ src/javascript/services.js
@@ -1,55 +1,77 @@
-'use strict';
+'use strict'
 
-require('angular-resource');
+require('angular-resource')
 
-module.exports = angular.module(
-  'flickrDupFinderServices',
-  ['ngResource', require('./config').name, require('./oauth-shim').name])
-  .service(
-    'Flickr',
-    ['$log', '$resource', '$http', '$q', '$location', 'OAuth', 'OAUTHD_URL', 'APP_PUBLIC_KEY',
-     function(
-       $log, $resource, $http, $q, $location, OAuth, OAUTHD_URL, APP_PUBLIC_KEY) {
-       if ($location.hash() === '') { $location.path('/photos'); } //so redirect to absUrl() works
-       OAuth.initialize(APP_PUBLIC_KEY, {cache: true});
-       OAuth.setOAuthdURL(OAUTHD_URL);
-       var resource = $q.defer();
-       function doneHandler(result) {
-         var key = APP_PUBLIC_KEY;
-         var oauthio = 'k=' + key;
-         oauthio += '&oauthv=1';
-         function kv_result(key) {
-           return '&'+key+'='+encodeURIComponent(result[key]);
-         }
-         oauthio += kv_result('oauth_token');
-         oauthio += kv_result('oauth_token_secret');
-         oauthio += kv_result('code');
-         $http.defaults.headers.common = {oauthio: oauthio};
-         resource.resolve(
-           $resource(
-             OAUTHD_URL + '/request/flickr/services/rest/',
-             {
-               method: "flickr.photos.search",
-               format: "json",
-               user_id: "me",
-               per_page: 10,
-               sort: 'date-taken-asc',
-               //text: "vision:outdoor",
-               //tags: "vision:outdoor,vision:outdoor=099",
-               //machine_tags: "outdoor",
-               extras: "date_upload,date_taken,tags",
-               nojsoncallback: 1
-             }));
-       }
+require('./config')
+require('./oauth-shim')
 
-       var oauthCallback = OAuth.callback('flickr');
-       if (oauthCallback) {
-         oauthCallback.done(doneHandler).fail(function(callbackError) {
-           $log.debug('OAuth.callback error: ', callbackError);
-         });
-       } else {
-         // the callback url must be routed through .otherwise in the app router
-         OAuth.redirect('flickr', $location.absUrl());
-       }
-       return resource.promise;
-     }]);
+module.exports = angular
+  .module('flickrDupFinderServices', [
+    'ngResource',
+    'flickrDupFinderConfig',
+    'OAuth',
+  ])
+  .service('Flickr', [
+    '$log',
+    '$resource',
+    '$http',
+    '$q',
+    '$location',
+    'OAuth',
+    'OAUTHD_URL',
+    'APP_PUBLIC_KEY',
+    function (
+      $log,
+      $resource,
+      $http,
+      $q,
+      $location,
+      OAuth,
+      OAUTHD_URL,
+      APP_PUBLIC_KEY
+    ) {
+      if ($location.hash() === '') {
+        $location.path('/photos')
+      } //so redirect to absUrl() works
+      OAuth.initialize(APP_PUBLIC_KEY, { cache: true })
+      OAuth.setOAuthdURL(OAUTHD_URL)
+      var resource = $q.defer()
+      function doneHandler(result) {
+        var key = APP_PUBLIC_KEY
+        var oauthio = 'k=' + key
+        oauthio += '&oauthv=1'
+        function kv_result(key) {
+          return '&' + key + '=' + encodeURIComponent(result[key])
+        }
+        oauthio += kv_result('oauth_token')
+        oauthio += kv_result('oauth_token_secret')
+        oauthio += kv_result('code')
+        $http.defaults.headers.common = { oauthio: oauthio }
+        resource.resolve(
+          $resource(OAUTHD_URL + '/request/flickr/services/rest/', {
+            method: 'flickr.photos.search',
+            format: 'json',
+            user_id: 'me',
+            per_page: 10,
+            sort: 'date-taken-asc',
+            //text: "vision:outdoor",
+            //tags: "vision:outdoor,vision:outdoor=099",
+            //machine_tags: "outdoor",
+            extras: 'date_upload,date_taken,tags',
+            nojsoncallback: 1,
+          })
+        )
+      }
+
+      var oauthCallback = OAuth.callback('flickr')
+      if (oauthCallback) {
+        oauthCallback.done(doneHandler).fail(function (callbackError) {
+          $log.debug('OAuth.callback error: ', callbackError)
+        })
+      } else {
+        // the callback url must be routed through .otherwise in the app router
+        OAuth.redirect('flickr', $location.absUrl())
+      }
+      return resource.promise
+    },
+  ])
