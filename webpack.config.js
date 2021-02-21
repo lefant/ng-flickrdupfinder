efc5d787e358b7474f834a774d02513c6b948759
Fabian Linzberger <e@lefant.net>
2021-02-21 18:59:59 +0100

    cleanup webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,12 @@
-const path = require('path')
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  node: {
-    // fs: false,
-  },
-  devServer: {
-    inline: false,
-    contentBase: './dist',
-  },
   resolve: {
     fallback: {
-      http: require.resolve('stream-http'),
-      // fs: false,
-      // util: require.resolve("util/"),
-      // path: require.resolve("path-browserify"),
+      buffer: require.resolve('buffer/'),
       crypto: require.resolve('crypto-browserify'),
-      // buffer: require.resolve("buffer/"),
+      http: require.resolve('stream-http'),
       https: require.resolve('https-browserify'),
       stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer/'),
       url: require.resolve('url/'),
-      // os: require.resolve("os-browserify/browser"),
-      // vm: require.resolve("vm-browserify"),
-      // constants: require.resolve("constants-browserify"),
-      // assert: require.resolve("assert/"),
     },
   },
 }
